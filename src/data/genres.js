f94444f3f00217108d60e1d0b5dd9dae36cd4dc0
CodewithAniket12 <aniketmadavi16@gmail.com>
2026-01-01 10:13:01 +0530

    feat : added genures.js which consist of data of genures stored as an object

---
+++ src/data/genres.js
@@ -0,0 +1,17 @@
+import FictionIcon from '../assets/icons/Fiction.svg';
+import DramaIcon from '../assets/icons/Drama.svg';
+import HumourIcon from '../assets/icons/Humour.svg';
+import PoliticsIcon from '../assets/icons/Politics.svg';
+import HistoryIcon from '../assets/icons/History.svg';
+import AdventureIcon from '../assets/icons/Adventure.svg';
+import PhilosophyIcon from '../assets/icons/Philosophy.svg';
+
+export var genres = [
+  { id: 'fiction', name: 'FICTION', icon: FictionIcon, path: '/books/fiction' },
+  { id: 'philosophy', name: 'PHILOSOPHY', icon: PhilosophyIcon, path: '/books/philosophy' },
+  { id: 'drama', name: 'DRAMA', icon: DramaIcon, path: '/books/drama' },
+  { id: 'history', name: 'HISTORY', icon: HistoryIcon, path: '/books/history' },
+  { id: 'humour', name: 'HUMOUR', icon: HumourIcon, path: '/books/humour' },
+  { id: 'adventure', name: 'ADVENTURE', icon: AdventureIcon, path: '/books/adventure' },
+  { id: 'politics', name: 'POLITICS', icon: PoliticsIcon, path: '/books/politics' }
+];
